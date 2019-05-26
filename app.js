da436ab12c68ebfae8ca9d66d843c08390378d4d
jingli <jingli.claire@gmail.com>
2019-05-26 17:59:15 +1000

    format form to toggle tabs for adding new income and expense

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+// toggle tabs
+// add new income
+document.getElementById('show-income-form-btn').addEventListener('click', showIncomeForm);
+
+function showIncomeForm() {
+  document.getElementById('exp-form').style.display = 'none';
+  document.getElementById('income-form').style.display = 'block';
+
+  let showIncomeFormBtn = document.getElementById('show-income-form-btn');
+  showIncomeFormBtn.style.backgroundColor = '#fecc06';
+  showIncomeFormBtn.style.color = '#ffffff';
+
+  let showExpenseFormBtn = document.getElementById('show-expense-form-btn');
+  showExpenseFormBtn.style.backgroundColor = '#ffffff';
+  showExpenseFormBtn.style.color = '#0A136A';
+
+  let formNav = document.getElementById('form-nav');
+  formNav.classList.remove('form-nav-expense-border');
+  formNav.classList.add('form-nav-income-border');
+}
+
+// add new expense
+document.getElementById('show-expense-form-btn').addEventListener('click', showExpenseForm);
+
+function showExpenseForm() {
+  document.getElementById('income-form').style.display = 'none';
+  document.getElementById('exp-form').style.display = 'block';
+
+  let showExpenseFormBtn = document.getElementById('show-expense-form-btn');
+  showExpenseFormBtn.style.backgroundColor = '#0A136A';
+  showExpenseFormBtn.style.color = '#ffffff';
+
+  let showIncomeFormBtn = document.getElementById('show-income-form-btn');
+  showIncomeFormBtn.style.backgroundColor = '#ffffff';
+  showIncomeFormBtn.style.color = '#fecc06';
+
+  let formNav = document.getElementById('form-nav');
+  formNav.classList.remove('form-nav-income-border');
+  formNav.classList.add('form-nav-expense-border');
+}
+
+
 //INCOME
 
 document.getElementById('income-save').addEventListener('click', saveIncome);
